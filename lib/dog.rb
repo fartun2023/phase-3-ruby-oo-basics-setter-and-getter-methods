98b85c6f7c4cab7a6afc2d5397c5869090e624ce
fartun <fartunwario34@gmail.com>
2023-05-18 21:35:54 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
+# lib/dog.rb
 class Dog
-
-end
+    def name=(name)
+      @name = name
+    end
+  
+    def name
+      @name
+    end
+  
+    def breed=(breed)
+      @breed = breed
+    end
+  
+    def breed
+      @breed
+    end
+  end
